8852300227dbc1cee827d856188c0fb75f05b763
gkaragoz <annotto3@gmail.com>
2017-05-29 00:32:14 +0300

    Create user login UI.

---
+++ MMORPG-Client/Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    public Button LoginButton;
+    public InputField NameInputField;
+
+    void Start() {
+        LoginButton.onClick.AddListener(delegate () { Login(); });
+	}
+
+    void Login()
+    {
+        string name = NameInputField.text;
+        Debug.Log(name);
+    }
+}
