b77ea484a4b4a1f7947979accc51270895ce38c7
gkaragoz <annotto3@gmail.com>
2017-05-28 23:08:50 +0300

    Finalize server index.js file.

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,48 @@ var io		= require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 3000);
 
+var clients = [];
 
+io.on("connection", function(socket){
+	var currentUser;
+	
+	socket.on("USER_CONNECT", function(){
+		console.log("User connected");
+		for(var i=0; i<clients.length; i++){
+			socket.emit("USER_CONNECTED", { name:clients[i].name, position:clients[i].position });
+			
+			console.log("User name " + cllients[i].name + " is connected.");
+		}
+	});
+	
+	socket.on("PLAY", function(data){
+		currentUser = {
+			name: data.name,
+			position: data.position
+		}
+		
+		clients.push(currentUser);
+		socket.emit("PLAY", currentUser);
+		socket.broadcast.emit("USER_CONNECTED", currentUser);
+	});
+	
+	socket.on("MOVE", function(data)){
+		currentUser.position = data.position;
+		socket.emit("MOVE", currentUser);
+		socket.broadcast.emit("MOVE", currentUser);
+		console.log(currentUser.name + " move to " + currentUser.position);
+	});
+	
+	socket.on("disconnect", function(){
+		socket.broadcast.emit("USER_DISCONNECTED", currentUser);
+		for (var i=0; i<clients.length; i++){
+			if(clients[i].name == currentUser.name){
+				console.log("User " + clients[i].name + " disconnected.";
+				clients.splice[i,1];
+			}
+		};
+	});
+});
 
 server.listen(app.get('port'), function(){
 	console.log("-------- SERVER IS RUNNING --------");
